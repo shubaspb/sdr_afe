9022ca3f5773a7b7c95379d66acc3176ed964075
Шубин Дмитрий <dspbrus@gmail.com>
2021-09-24 14:41:31 +0300

    Update read_me.tcl

--- read_me.tcl
+++ read_me.tcl
@@ -1,29 +1,27 @@
 
 restore xilinx-project:
-	cd D:/WORK/afe7700_xilinx
-	source D:/WORK/afe7700_xilinx/restore_project.tcl
-	import
+	source restore_project.tcl
 
 
 AFE7700 programming:
-	- program fpga by jtag - https://git.npp-crts.ru/fpga/afe7700_xilinx/tree/master/firmware
+	- program fpga by jtag - /firmware
 	- reset AFE7700
 	- LATTE:
 		- setup.py
 		- devinit.py
 	        - ti_functions.py
 		- shubin_tcas.py
-		- program fpga или сброс jesd phy fpga
+		- program fpga пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ jesd phy fpga
 		- forceSync(1),forceSync(0)
 	- TX QMC correction
 	        - connect LPF to the cable, create loop from TX to FB1  
 		- doTxIqmc.py , where type particular TX channel
 	        - check result by the spectrum analiser
 	- hardware configuration:
-		- сброс питания
+		- пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 		- setup.py
 		- devInit.py
 		- sequencer_custom.py
 		- shubin_conf_script.py
-	        - сброс fpga phy jesd  (SW14 on the  ZCU102)
+	        - пїЅпїЅпїЅпїЅпїЅ fpga phy jesd  (SW14 on the  ZCU102)
 		- forceSync(1),forceSync(0)
